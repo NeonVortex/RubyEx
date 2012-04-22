489c3a86ba975c4a0b7dc9844bde8a2841793acd
texasbruce <popoxee@hotmail.com>
2012-04-22 16:11:53 -0400

    Changed DepHash and Hash.new'

--- hash-ex.rb
+++ hash-ex.rb
@@ -1,8 +1,22 @@
 class Hash
 	
-	def + (appendant)
+	def +(appendant)
 		raise ArgumentError, "Type mismatch: #{appendant.name} does not correspond to hash." unless appendant.respond_to?(:to_hash)
 		self.merge(appendant.to_hash)
 	end
 	
+	class << self
+	  alias_method :__new__,:new
+	  def new(obj=nil,&block)
+	    if obj && block && block.arity == 1
+	      rval = __new__(obj)
+	      yield rval
+	      rval
+	    else
+	      __new__(obj,&block)
+	    end
+	  end
+	end
+	
 end
+
